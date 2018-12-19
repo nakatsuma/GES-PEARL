ac863cafb007d61027dd3e789f9529635f283550
Teruo Nakatsuma <nakatuma@econ.keio.ac.jp>
2018-12-20 00:54:52 +0900

    Add graphs

--- ges_capm.py
+++ ges_capm.py
@@ -1,13 +1,24 @@
 import numpy as np
 import scipy.linalg as la
 import pandas as pd
+import matplotlib.pyplot as plt
 stockvalue = pd.read_csv('capm.csv', index_col=0)
 R = (stockvalue.diff()/stockvalue.shift(1))[1:] * 100
 R.index = pd.date_range('2013-4-1', periods=R.shape[0], freq='M')
 Y = R['TOPIX']
 del R['TOPIX']
-T = R.shape[0]
+T, N = R.shape
 StockList = R.columns
 Coefs = pd.DataFrame([la.lstsq(np.vstack((np.ones(T), Y)).T, R[Stock])[0] \
                      for Stock in StockList],
                      index=StockList, columns=['Alpha','Beta'])
+fig, ax = plt.subplots(N, 2, sharex='col', facecolor='w')
+ax[0, 0].set_title('Time Series Plot')
+ax[0, 1].set_title('Scatter Plot')
+for index, Stock in enumerate(StockList):
+    ax[index, 0].plot(R[Stock])
+    ax[index, 0].set_ylabel(Stock)
+    ax[index, 1].plot(Y, R[Stock], '+')
+ax[-1, 1].set_xlabel('TOPIX')
+plt.tight_layout()
+plt.show()
