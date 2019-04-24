f707b67501baf13f9836bfb88f33d93c365ace81
Teruo Nakatsuma <nakatuma@econ.keio.ac.jp>
2019-04-24 12:53:55 +0900

    r_[] -> hstack()

--- ges_interest.py
+++ ges_interest.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 r = 0.2
 Maturity = 10
 Simple_Rate = 1.0 + r * np.linspace(0, Maturity, Maturity + 1)
-Compound_1year = np.r_[1.0, np.cumprod(np.tile(1.0 + r, Maturity))]
-Compound_6month = np.r_[1.0, np.cumprod(np.tile((1.0 + r/2.0)**2, Maturity))]
+Compound_1year = np.hstack((1.0, np.cumprod(np.tile(1.0 + r, Maturity))))
+Compound_6month = np.hstack((1.0, np.cumprod(np.tile((1.0 + r/2.0)**2, Maturity))))
 Continuous_Rate = np.exp(r*np.linspace(0, Maturity, Maturity + 1))
 fig1 = plt.figure(num=1, facecolor='w')
 plt.plot(Simple_Rate, 'b-')
